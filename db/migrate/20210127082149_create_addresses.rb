e48015bc546f473882aa099b2e8445b86272f7ec
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-01-27 19:04:04 +0900

    addressのカラム設定

--- db/migrate/20210127082149_create_addresses.rb
+++ db/migrate/20210127082149_create_addresses.rb
@@ -1,8 +1,15 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.string :tel, uniqueness: true
+      t.string :post_code, null: false
+      t.integer :prefecture, null: false
+      t.string :city, null: false
+      t.string :block_number, null: false
+      t.string :building_name
+      t.integer :user_id, null: false, foreign_key: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_073130) do
+ActiveRecord::Schema.define(version: 2021_01_27_082149) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "tel"
+    t.string "post_code", null: false
+    t.integer "prefecture", null: false
+    t.string "city", null: false
+    t.string "block_number", null: false
+    t.string "building_name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
