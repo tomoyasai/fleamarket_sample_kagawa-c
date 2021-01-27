dea4de487762d40ee73ee7c5b3987cbb611d1ce5
tomoyasai <sugi97421yama84619@gmail.com>
2021-01-27 11:57:01 +0900

    Update 20210126081230_create_items.rb

--- db/migrate/20210126081230_create_items.rb
+++ db/migrate/20210126081230_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.text :info, null: false
+      t.references :category, null: false, foreign_key: true
+      t.intger :status_id, null: false
+      t.intger :delivery_fee_id, null: false
+      t.intger :elivery_area_id, null: false
+      t.intger :delivery_days_id, null: false
+      t.intger :user_id, null: false, foreign_key: true
+      t.intger :price, null: false
       t.timestamps
     end
   end
