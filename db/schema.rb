4c399e2da10e35d722691a9bd5fb5d7ddbd54e28
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-01-29 19:34:42 +0900

    DBカラム名修正
    prefecture_idに変更

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_082149) do
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tel"
     t.string "post_code", null: false
-    t.integer "prefecture", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "block_number", null: false
     t.string "building_name"
