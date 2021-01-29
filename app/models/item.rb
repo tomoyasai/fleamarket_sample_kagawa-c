ae17cd60f5191a138612e76b0b00760a05c23ff7
tomoyasai <sugi97421yama84619@gmail.com>
2021-01-29 14:23:34 +0900

    blongs_to:statusに変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ApplicationRecord
   has_many :comments
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :condition
+  belongs_to_active_hash :status
   validates :content, presence: true
 end
