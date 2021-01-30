d0d95942fdf362074eefbb5d93e2b8e57e10b998
tomoyasai <sugi97421yama84619@gmail.com>
2021-01-30 16:56:38 +0900

    imageテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_081350) do
+ActiveRecord::Schema.define(version: 2021_01_26_081230) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,15 +19,9 @@ ActiveRecord::Schema.define(version: 2021_01_26_081350) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
-    t.integer "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "image", null: false
     t.text "info", null: false
     t.bigint "category_id", null: false
     t.integer "status_id", null: false
