d3d90a054e1f9321916f9be0d967249eb793a7ac
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-02-01 16:52:49 +0900

    FactoryBotファイル作成中
    今はaddressテーブルの内容もいっしょに記述している。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,24 +1,25 @@
 FactoryBot.define do
 
   factory :user do
-    nick_name              {"abe"}
+    nick_name             {"ニックネーム"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
-    birthday              {}
-    family                {}
-    first                 {}
-    family_kana           {}
-    first_kana            {}
-    
+    birthday              {"2020-01-01"}
+    family                {"佐藤"}
+    first                 {"太郎"}
+    family_kana           {"サトウ"}
+    first_kana            {"タロウ"}
   end
 
   factory :address do
     post_code              {"abe"}
-    prefecture_id                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    prefecture_id          {1}
+    city                   {"高松テスト市"}
+    block_number           {"テスト町"}
+    building_name          {"テストアパート"}
+    tel                    {"000-0000-0000"}
+    user_id                    {"11"}
   end
-
 
 end
