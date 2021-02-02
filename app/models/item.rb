148a58d67af29858a2e2a9ab439c170f808949de
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-02 14:11:56 +0900

    itemモデルのバリデーションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buy_data
-  has_many :images
+  # has_many :images
   belongs_to :category
   has_many :comments
   mount_uploader :image, ImageUploader
-  # validates :name, presence: true, uniqueness: true
-  # validates :content, presence: true, unless: :image?
-
+  
+  validates :name, presence: true, uniqueness: true
+  validates :info, presence: true
+  validates :category, presence: true
+  validates :status_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :user_id, presence: true
+  validates :price, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
