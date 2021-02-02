f993faa2e2aa965a8cdef051e36a09386bf89df2
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-02 15:40:26 +0900

    バリデーションをまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,20 +5,22 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many :comments
   mount_uploader :image, ImageUploader
-  
-  validates :name, presence: true, uniqueness: true
-  validates :info, presence: true
-  validates :category, presence: true
-  validates :status_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_days_id, presence: true
-  validates :user_id, presence: true
-  validates :price, presence: true
+
+  with_options presence: true do
+    validates :name, uniqueness: true
+    validates :info
+    validates :category
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_days_id
+    validates :user_id
+    validates :price
+    validates :image
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :status
-
-
+  
 end
