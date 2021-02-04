5915441fc91e2f72191ce76913d51b3d740df329
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-04 17:49:15 +0900

    テストファイルの修正

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    # nick_name {a}
+    # email {b@b}
+    # encrypted_password {123abc}
+    # birthday{1111}
+    # family{a}
+    # first{a}
+    # family_kana{a}
+    # first_kana{a}
+    # association :item
+  end
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
-describe Item do
+RSpec.describe Item, type: :model do
   describe '#create' do
-
-    it "is invalid without a name" do
-      item = build(:item, name: "")
-      item.valid?
-      expect(item.errors[:name]).to include("can't be blank")
+    # before do
+    #   user = FactoryBot.create(:user)
+    #   item = FactoryBot.build(:item, user_id: user.id)
+    # end
+    it "商品出品ができる時" do
+      @item = build(:item)
+      expect(@item).to be_valid
     end
+
+    # it "is invalid without a info" do
+    #   @item = FactoryBot.create(:item, info: "")
+    #   @item.valid?
+    #   expect(@item.errors.full_messages).to include("can't be blank")
+    # end
   end
 end
+# describe Product do
+#   describe '#create' do
+#     it "is valid with a name, price, description, status, size, days, cost, prefecture_id, category_id, user_id" do
+#       product = FactoryBot.build(:product)
+#       expect(product).to be_valid
+#     end
+#   end
+# end
