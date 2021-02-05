04a234f060ffdf5acbf4667af626874461ba8242
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-05 11:33:12 +0900

    ファクトリーファイルの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     name { "aa" }
     info  { "bb" }             
     image { File.open("#{Rails.root}/public/images/test_image.jpg") }
-    category_id { 1 }
     status_id { 1 }
     delivery_fee_id { 1 }
     delivery_days_id { 1 }
     prefecture_id { 1 }
     price  { "1000" }
-    user_id { 1 } 
+    user
+    category 
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
   factory :user do
-  #   nick_name {a}
-  #   email {b@b}
-  #   encrypted_password {123abc}
-  #   birthday{1111}
-  #   family{a}
-  #   first{a}
-  #   family_kana{a}
-  #   first_kana{a}
-  #   association :item
+    email {"basas@bafdaef"}
+    password {"123abc"}
+    
   end
 end
