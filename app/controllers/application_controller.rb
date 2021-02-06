b3caf1937720fcbd8c2395ae289919f6ee2fc847
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-02-07 00:27:00 +0900

    コードレビュー３回目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def create
-    Team.create(team_params)
-  end
-  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :birthday, :family, :first, :family_kana, :first_kana, address_attributes:[:post_code, :prefecture_id, :city, :block_number, :building_name, :tel, :user_id]])
