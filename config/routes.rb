e5181d723b5a88cfd0ad58ec3b90e171e1f92974
junyakonishi <jmmk318@gmail.com>
2021-02-10 11:50:46 +0900

    商品詳細削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
   }
   root 'items#index'
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :destroy]
   get 'mypage',to: 'users#show'
   get 'api/items/category',to: 'items#get_category'
 
