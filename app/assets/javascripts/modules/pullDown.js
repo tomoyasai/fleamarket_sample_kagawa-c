9f52fd7f04324d9fbfa31089ef4d5f31c2417eed
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-11 17:49:30 +0900

    JSを記述

---
+++ app/assets/javascripts/modules/pullDown.js
@@ -0,0 +1,51 @@
+$(function(){
+
+  function buildForm(categories){
+    let options='';
+    categories.forEach(function (category) { 
+      options += `
+                  <option value="${category.name}">${category.name}</option>
+                 `;
+    });
+
+    
+    const html=`
+      <select class="form-pull-down select-category"  name="category_id">
+        <option value="">---</option>
+        ${options}
+      </select>
+    `
+    return html
+  }
+
+
+//changeイベント設定箇所
+  $('.base__main__detail__category').on('change','.select-category',function(){
+    alert('change!')
+    $(this).nextAll().remove()  //選択された要素より下のformを一度リセット
+    const category_id=$(this).val() //選択されたidを取得
+
+  //Ajaxのリクエストを記述
+    $.ajax({
+      type: "GET",
+      url: "/api/items/category",
+      data: {category_id: category_id },
+      dataType: 'json'
+    })
+    .done(function(categories) {
+
+  
+      if(categories.length==0){ //レスポンスでカテゴリーがない場合は処理を行わない
+        return false
+      }
+      
+      const select_form=buildForm(categories) //挿入するフィームを作成
+      const target=$('.base__main__detail__category') //append先である既存の要素を定義
+      target.append(select_form)
+    
+    })
+    .fail(function() {
+      alert('失敗')
+    });
+  })
+});
